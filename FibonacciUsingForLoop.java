16b0c8e04cf0bc155ad3f381531db51f47f9a0b6
Purabi Das <32848180+PurabiDas@users.noreply.github.com>
2023-07-04 21:15:21 +0530

    Create FibonacciUsingForLoop.java

---
+++ FibonacciUsingForLoop.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;  
+
+class fibonacci {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int firstTerm = 0, secondTerm = 1;
+    System.out.println("Fibonacci Series till " + n + " terms:");
+    for (int i = 1; i <= n; ++i) {
+      System.out.print(firstTerm + ", ");
+      int nextTerm = firstTerm + secondTerm;
+      firstTerm = secondTerm;
+      secondTerm = nextTerm;
+    }
+  }
+}
